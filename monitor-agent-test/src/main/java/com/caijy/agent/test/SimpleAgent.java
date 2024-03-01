86d356ec0578b8334d0daa2683ace2be81834eef
caijy <c18268151702@163.com>
2024-03-01 14:38:27 +0800

    update: 测试

--- monitor-agent-test/src/main/java/com/caijy/agent/test/SimpleAgent.java
+++ monitor-agent-test/src/main/java/com/caijy/agent/test/SimpleAgent.java
@@ -1,13 +1,28 @@
 package com.caijy.agent.test;
 
+import com.caijy.agent.core.plugin.interceptor.enhance.EnhancedInstance;
 import com.caijy.agent.core.plugin.interceptor.enhance.InstrumentMethodInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.implementation.FieldAccessor;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.matcher.ElementMatchers;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
+import java.net.URL;
+import java.security.ProtectionDomain;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.jar.JarFile;
+
+import static net.bytebuddy.jar.asm.Opcodes.ACC_PRIVATE;
+import static net.bytebuddy.jar.asm.Opcodes.ACC_VOLATILE;
 
 /**
  * @author caijy
@@ -16,14 +31,78 @@ import java.lang.instrument.Instrumentation;
  */
 @Slf4j
 public class SimpleAgent {
+    public static final String CONTEXT_ATTR_NAME = "_$EnhancedClassField_ws";
+
     public static void premain(String agentArgs, Instrumentation inst) {
-        log.debug("SimpleAgent....premain");
+        System.out.println("SimpleAgent....premain");
         new AgentBuilder.Default()
-                .type(ElementMatchers.named("org.apache.dubbo.monitor.support.MonitorFilter"))
+                .type(ElementMatchers.named("com.xiaobaicai.domain.dto.UserDto").or(ElementMatchers.named("com.xiaobaicai.api.impl.UserServiceImpl")))
                 .transform((builder, type, classLoader, module) ->
-                        builder.method(ElementMatchers.named("invoke"))
-                                .intercept(MethodDelegation.to(new InstrumentMethodInterceptor(new DubboInterceptorV1()))))
+                {
+                    builder = builder.defineField(
+                            CONTEXT_ATTR_NAME, Object.class, ACC_PRIVATE | ACC_VOLATILE)
+                            .implement(EnhancedInstance.class)
+                            .intercept(FieldAccessor.ofField(CONTEXT_ATTR_NAME));
+                    return builder;
+                })
                 .with(AgentBuilder.RedefinitionStrategy.REDEFINITION)
                 .installOn(inst);
+    }
+
+    public static void agentmain(String agentArgs, Instrumentation inst) throws Exception {
+        System.out.println("SimpleAgent....agentmain");
+        // 添加到启动类加载器，为了让所有classloader能加载到agent包中的类
+        String clazzName = SimpleAgent.class.getName().replace(".", "/") + ".class";
+        URL resource = ClassLoader.getSystemClassLoader().getResource(clazzName);
+        if (resource.getProtocol().equals("jar")) {
+            int index = resource.getPath().indexOf("!/");
+            if (index > -1) {
+                String jarFile = resource.getPath().substring("file:".length(), index);
+                inst.appendToBootstrapClassLoaderSearch(new JarFile(new File(jarFile)));
+            }
+        }
+
+        DemoClassFileTransformer transformer = new DemoClassFileTransformer();
+        inst.addTransformer(transformer, true);
+
+        Map<String, ClassLoader> classLoaderMap = new HashMap<>();
+
+        for (Class loadedClass : inst.getAllLoadedClasses()) {
+            classLoaderMap.put(loadedClass.getName(), loadedClass.getClassLoader());
+        }
+
+        String className = "com.xiaobaicai.api.impl.UserServiceImpl";
+        ClassLoader loader = classLoaderMap.get(className);
+        Class<?> loadClass = loader.loadClass(className);
+        inst.retransformClasses(loadClass);
+
+    }
+
+    public static class DemoClassFileTransformer implements ClassFileTransformer {
+
+        @Override
+        public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+            if (classfileBuffer != null) {
+                String dir = "/Users/jianyingcai/IdeaProjects/practice/Service-Invocation-Monitor-Demo/lib/dump";
+                File tmpFile = new File(dir + File.separator + className.replaceAll("/", ".") + ".class");
+                try {
+                    if (!tmpFile.exists()) {
+                        String parent = tmpFile.getParent();
+                        File file = new File(parent);
+                        if (!file.exists()) {
+                            file.mkdirs();
+                        }
+                        tmpFile.createNewFile();
+                    }
+                    // 写入文件
+                    FileOutputStream fileOutputStream = new FileOutputStream(tmpFile);
+                    fileOutputStream.write(classfileBuffer);
+                    fileOutputStream.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return classfileBuffer;
+        }
     }
 }
